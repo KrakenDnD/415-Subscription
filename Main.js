e6644e7191eca00621257fa96efdd7088b7a000d
sbancevic <98131065+sbancevic@users.noreply.github.com>
2024-04-28 16:16:33 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -4,6 +4,10 @@ const cookieParser = require('cookie-parser');
 const ObjectId = require('mongodb').ObjectId;
 const session = require('express-session');
 const Database = require('./dataContext');
+const CommentObserver = require('./Observer');
+const CommentModel = require('./CommentModel');
+const CommentView = require('./CommentView');
+const CommentController = require('./CommentController');
 
 
 const app = express();
