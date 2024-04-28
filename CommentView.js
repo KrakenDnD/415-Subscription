d73dd49fbb31aacb56559aea191b69e61142f0f4
sbancevic <98131065+sbancevic@users.noreply.github.com>
2024-04-27 22:59:01 -0500

    Create CommentView.js

---
+++ CommentView.js
@@ -0,0 +1,16 @@
+// views/CommentView.js
+class CommentView {
+  static render(comment) {
+    return `
+      <div>
+        <strong>Topic:</strong> ${comment.topicID}<br>
+        <strong>Date:</strong> ${new Date(comment.dateTime).toLocaleString()}
+        <br>
+        <div><strong>User:</strong> ${comment.userID}<br>
+        <strong>Comment:</strong> ${comment.commentContent}</div><br>
+      </div>
+    `;
+  }
+}
+
+module.exports = CommentView;
