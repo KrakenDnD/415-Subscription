861cf0d2b136c69c88db6d03f2b49fa8494de561
sbancevic <98131065+sbancevic@users.noreply.github.com>
2024-04-27 22:58:05 -0500

    Create CommentModel.js

---
+++ CommentModel.js
@@ -0,0 +1,12 @@
+// models/CommentModel.js
+class Comment {
+  constructor(commentID, topicID, commentContent, userID, dateTime) {
+    this.commentID = commentID;
+    this.topicID = topicID;
+    this.commentContent = commentContent;
+    this.userID = userID;
+    this.dateTime = dateTime;
+  }
+}
+
+module.exports = Comment;
